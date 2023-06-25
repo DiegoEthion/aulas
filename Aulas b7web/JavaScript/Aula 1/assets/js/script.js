a6d446af72b8c0662b6e12f18c99966af03e8996
Diego Gonçalves <diegobass55@hotmail.com>
2023-06-24 21:47:08 -0300

    Inicia JS B7web

---
+++ Aulas b7web/JavaScript/Aula 1/assets/js/script.js
@@ -0,0 +1,26 @@
+let nome = "Diego Oliveira Gonçalves"
+let idade = 34
+let logado = true
+let lista = ["um","dois", "tres"] //array
+let nomeCompleto = {Nome:nome, Idade:idade}
+
+console.log(nome)
+console.log(idade + " Anos")
+
+
+if (logado === true) {
+    console.log("Bom dia senhor " + nome)
+    document.getElementById('titulo').innerHTML = ("Bom dia senhor " + nome + "de " + idade + " anos");
+
+}else{
+    console.log("voce não está logado por favor caça login " + nome)
+    document.getElementById('titulo').innerHTML = ("Você não está logado por favor faça login " + nome)
+
+}   
+console.log("o tipo da variavel " + nome + " é " + typeof nome) 
+console.log(nomeCompleto)
+
+// comentarios
+// de 
+// multiplas 
+// linhas
